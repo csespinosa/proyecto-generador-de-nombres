ecc432e4a89619efaff3b23934af0f989a0f7621
Clara Espinosa Pantoja <claritapantoja36@gmail.com>
2024-12-11 20:50:58 +0000

    proyecto generador dominiios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "hers"];
+let adj = ["great", "big", "shinny"];
+let noun = ["jogger", "racoon", "butterfly"];
+let ext = [".com", ".net", ".org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let a = 0; a < pronoun.length; a++) {
+  for (let b = 0; b < adj.length; b++) {
+    for (let c = 0; c < noun.length; c++) {
+      for (let d = 0; d < ext.length; d++) {
+        // eslint-disable-next-line no-console
+        console.log(pronoun[a] + adj[b] + noun[c] + ext[d]);
+      }
+    }
+  }
+}
